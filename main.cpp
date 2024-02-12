a006b953d153ec287b0518039aa20313a8f8850a
Vilyanare <acolyteofwar@gmail.com>
2024-02-12 13:12:59 -0800

    fix: refactored code to use 2d vectors instead of arrays

--- main.cpp
+++ main.cpp
@@ -1,56 +1,66 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+#include<vector>
 using namespace std;
 
-/**/
+/*GAME OF LIFE
+Simulation of cells based on 4 rules
+1. Any live cell with 0 or 1 live neighbors becomes dead, because of underpopulation
+2. Any live cell with 2 or 3 live neighbors stays alive, because its neighborhood is just right
+3. Any live cell with more than 3 live neighbors becomes dead, because of overpopulation
+4. Any dead cell with exactly 3 live neighbors becomes alive, by reproduction*/
 
-//Function that initializes a board to all 0's
-int *dead_state (int *board, int height, int width) {
-    memset(board, 0, (height * width) * sizeof(int));
-    return board;
-}
-
-//Function that initializes a board with random states
-int *random_state (int *board, int height, int width) {
-    srand(time(0));
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-            board[(x * width) + y] = rand() % 2;
+//Function that sets a board to all 0's
+void dead_state (vector<vector<int>> board) {
+    for(int x = 0; x < board.size(); x++) {
+        for (int y = 0; y < board[0].size(); y++) {
+            board[x][y] = 0;
         }
     }
-    return board;
+}
+
+//Function that sets all board variables to random states
+void random_state (vector<vector<int>> board) {
+    srand(time(0));
+    for (int x = 0; x < board.size(); x++) {
+        for (int y = 0; y < board[0].size(); y++) {
+            board[x][y] = abs(rand() % 2);
+        }
+    }
 }
 
 //Function to pretty print a board state.  1 = # 0 = " "
-void render_board(int *board, int height, int width) {
-    for (int x = 0; x < width + 2; x++) {
+void render_board(vector<vector<int>> board) {
+    for (int x = 0; x < board.size() + 2; x++) {
         cout << "-";
     }
     cout << endl;
-    for (int x = 0; x < width; x++)  {
+    for (int x = 0; x < board.size(); x++)  {
         cout << "|";
-        for (int y = 0; y < height; y++) {
-            cout << ((board[(x * width) + y]) ? "#" : " ");
+        for (int y = 0; y < board[0].size(); y++) {
+            cout << board[x][y] ? "#" : " ";
         }
         cout << "|" << endl;
     }
-    for (int x = 0; x < width + 2; x++) {
+    for (int x = 0; x < board.size() + 2; x++) {
         cout << "-";
     }
     cout << endl;
 }
 
+//Function that calculates the next board based on the 4 rules and updates board.
+void next_board_state(vector<vector<int>> board) {
+}
+
 int main () {
-    int width = 5, height = 5;
-    int board[width][height];
-    random_state(*board, height, width);
-    render_board(*board, height, width);
-    for (int x = 0; x < width; x++) {
-        for (int y = 0; y < height; y++) {
-            cout << board[x][y] << " ";
-        }
-        cout << endl;
-    }
+    const int width = 5, height = 5;
+    vector<int> row(width, 0);
+    vector<vector<int>> board(height, row);
+    dead_state(board);
+    random_state(board);
+    render_board(board);
+    next_board_state(board);
+    render_board(board);
     return 0;
 }
