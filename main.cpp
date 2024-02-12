1005eb7547682dbe7736339938869d5a6854c593
Vilyanare <acolyteofwar@gmail.com>
2024-02-12 13:33:27 -0800

    fix:  board wasn't being updated in functions.

--- main.cpp
+++ main.cpp
@@ -12,22 +12,24 @@ Simulation of cells based on 4 rules
 4. Any dead cell with exactly 3 live neighbors becomes alive, by reproduction*/
 
 //Function that sets a board to all 0's
-void dead_state (vector<vector<int>> board) {
+vector<vector<int>> dead_state (vector<vector<int>> board) {
     for(int x = 0; x < board.size(); x++) {
         for (int y = 0; y < board[0].size(); y++) {
             board[x][y] = 0;
         }
     }
+    return board;
 }
 
 //Function that sets all board variables to random states
-void random_state (vector<vector<int>> board) {
+vector<vector<int>> random_state (vector<vector<int>> board) {
     srand(time(0));
     for (int x = 0; x < board.size(); x++) {
         for (int y = 0; y < board[0].size(); y++) {
             board[x][y] = abs(rand() % 2);
         }
     }
+    return board;
 }
 
 //Function to pretty print a board state.  1 = # 0 = " "
@@ -39,7 +41,8 @@ void render_board(vector<vector<int>> board) {
     for (int x = 0; x < board.size(); x++)  {
         cout << "|";
         for (int y = 0; y < board[0].size(); y++) {
-            cout << board[x][y] ? "#" : " ";
+            char state = board[x][y] ? '#' : ' ';
+            cout << state;
         }
         cout << "|" << endl;
     }
@@ -50,17 +53,18 @@ void render_board(vector<vector<int>> board) {
 }
 
 //Function that calculates the next board based on the 4 rules and updates board.
-void next_board_state(vector<vector<int>> board) {
+vector<vector<int>> next_board_state(vector<vector<int>> board) {
+    return board;
 }
 
 int main () {
     const int width = 5, height = 5;
     vector<int> row(width, 0);
     vector<vector<int>> board(height, row);
-    dead_state(board);
-    random_state(board);
+    board = dead_state(board);
+    board = random_state(board);
     render_board(board);
-    next_board_state(board);
+    board = next_board_state(board);
     render_board(board);
     return 0;
 }
